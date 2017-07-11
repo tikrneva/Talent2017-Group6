2130970eb71cff2f1f7deba0da42c28a13e435e0
tikrneva <tiia.k.haverinen@student.jyu.fi>
2017-07-11 17:19:01 +0300

    "Two particles on the same state"-check added

--- CheckDir/permutator_a_la_tiia.py
+++ CheckDir/permutator_a_la_tiia.py
@@ -6,30 +6,50 @@ import numpy as np
 
 # This function permutates array and gives the phase +1 or -1
 # depending on the amount of moves in permutation
-# TODO: this does NOT check if there are two same values in the array?
+# RETURNS sign from the permutation and the ordered array
 def permutator(array):
     alist = array.tolist()
     nummov=0 #number of moves
+    #------------------------------------------------------
+    # at first we check that there are no two same elements
+    seen = []
+    for number in alist:
+        if number in seen:
+            print "===================================================="
+            print "             E R R O R !!!!                     "
+            print " Pauli is angry - two particles on the same state!"
+            print "===================================================="
+            return 0,[]  
+        else:
+            seen.append(number)
+       #The list 'seen' not needed later
+    #--------------------------------------------------------
+    # PERMUTATOR ITSELF
+    #--------------------------------------------------------
     if len(alist)!=0:
        for i in range(0, len(alist)):
            minval=min(alist[i:]) #find min. value between i:th position
                                  # and the last (first i states have already
                                  # been permutated
-           minindex=alist.index(minval)
+           minindex=alist.index(minval) # store the index of minval
            nummov=nummov+(minindex-i) #update number of moves needed
            alist.remove(minval) #remove minval from the list
            alist.insert(i,minval) #insert minval in the position i
        ordarray=np.asarray(alist)
+       # Even number of moves -> +1, odd number of moves -> -1
        if nummov%2==0:
            sign=1
        else: 
            sign=-1
        return sign,ordarray
+    # if len(alist) == 0 then:
     else:
        return 0,array #array is empty
 
 # Just testing, these can be removed =============>
-myarr1 = np.array([1,3,4,5])
-myorderedarray = np.array([1,3,4,5])
+myarr1 = np.array([5,3,1,1])
+myarr2 = np.array([5,3,1,2])
 testsign, testarray = permutator(myarr1)
 print testsign, testarray
+testsign2, testarray2 = permutator(myarr2)
+print testsign2, testarray2
