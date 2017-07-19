b9db8bbe7cd20a1b2279a438e131985d9deb293b
shanew20 <shane.g.wilkins@gmail.com>
2017-07-19 16:33:47 +0200

    This is a function that will return excitation energies of levels calculated with nushellx

---
+++ Project/oxygen_data/read_lpt.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_lpt(N_particles):
+	data = np.loadtxt('o_'+str(16+N_particles)+'2.lpt',skiprows=6,usecols=[0,1,2,3,4,5,6])
+
+	number = []
+	nth_J = []
+	E = []
+	E_ex = []
+	J = []
+	T_z = []
+	parity = []
+
+	for datum in data:
+		number.append(datum[0])
+		nth_J.append(datum[1])
+		E.append(datum[2])
+		E_ex.append(datum[3])
+		J.append(datum[4])
+		T_z.append(datum[5])
+		parity.append(datum[6])
+
+	number = np.array(number)
+	nth_J = np.array(nth_J)
+	E = np.array(E)
+	E_ex = np.array(E_ex)
+	J = np.array(J)
+	T_z = np.array(T_z)
+	parity = np.array(parity)
+
+	return E_ex
+
+
+
